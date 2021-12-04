b2a38e97388e20f67e84b44626626700e416c0e8
surekhabhadane <surekhabhadane1304@gmail.com>
2021-12-04 18:36:59 +0530

    initial commit

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+const env = process.env.NODE_ENV || 'development'
+
+type Config = {
+  serverUrl: string
+  serverPort: number
+  serverDatabase: string
+  jwtSecret: string
+  whitelist?: string
+}
+
+const config: Config = require(`./${env}`).default
+
+export default config
